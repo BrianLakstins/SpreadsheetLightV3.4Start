3194b1b290aeb5b174d03d6f66fb4078636cba7d
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2025-04-16 17:39:34 -0400

    Add .NET Solution and .NET Project for .NET 4.5.2

---
+++ SpreadsheetLight-NF-4.5.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Lakstins Family, LLC">
+// Copyright (c) 2011 Vincent Tan
+// </copyright>
+
+#region License
+// <license>
+// This software is provided 'as-is', without any express or implied warranty. In no 
+// event will the author be held liable for any damages arising from the use of this 
+// software.
+//  
+// Permission is granted to anyone to use this software for any purpose, including 
+// commercial applications, and to alter it and redistribute it freely, subject to the 
+// following restrictions:
+// 
+// 1. The origin of this software must not be misrepresented; you must not claim that 
+// you wrote the original software. If you use this software in a product, an 
+// acknowledgment (see the following) in the product documentation is required.
+// 
+// Portions Copyright (c) Brian A. Lakstins (http://www.lakstins.com/brian/)
+// 
+// 2. Altered source versions must be plainly marked as such, and must not be 
+// misrepresented as being the original software.
+// 
+// 3. This notice may not be removed or altered from any source distribution.
+// </license>
+#endregion
+
+#region Change Log
+// <changelog>
+// <change date="4/16/2025" author="Brian A. Lakstins" description="Initial creation">
+// </changelog>
+#endregion
+
+using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+#if DEBUG
+[assembly: AssemblyTitle("Debug - SpreadsheetLight .Net 4.5.2")]
+[assembly: AssemblyDescription("Debug - SpreadsheetLight Library .Net 4.5.2")]
+[assembly: AssemblyProduct("Debug - SpreadsheetLight Library .Net 4.5.2")]
+#else
+[assembly: AssemblyTitle("SpreadsheetLight .Net 4.5.2")]
+[assembly: AssemblyDescription("SpreadsheetLight Library .Net 4.5.2")]
+[assembly: AssemblyProduct("SpreadsheetLight Library .Net 4.5.2")]
+#endif
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lakstins Family, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2011 Vincent Tan")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2025.04.16.01")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyInformationalVersion("2025.04.16.01")]
